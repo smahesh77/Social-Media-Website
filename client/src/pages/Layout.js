07640684fd90a6ef167ce154e4bf26ea9491eddc
Niya Shiyas <niyashiyas03@gmail.com>
2023-05-24 06:25:52 +0530

    frontend changed

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -1,13 +1,42 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 function Layout() {
+  // return (
+
+  const [listOfPosts, setListOfPosts] = useState([]); // gets the data from server in useEffect and stores in into list of posts
+  let navigate = useNavigate(); // exactly like navigator
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setListOfPosts(response.data); // this will somehow send the data to listpost, dw about it now
+    });
+  }, []);
+  // to make the new blogs come first
+  const reversedPosts = [...listOfPosts].reverse(); //value will go through each object in the list
   return (
     <>
       <div>
         <h1>Layout</h1>
         <p>This is main page</p>
-        <div className="stats-con"></div>
+        <div>
+          {reversedPosts.map((value, key) => {
+            return (
+              <div
+                className="post"
+                onClick={() => {
+                  navigate(`/post/${value.id}`);
+                }}
+              >
+                {/**navigator.push (where you want to go the route) the route must be specified*/}
+                <div className="title"> {value.title} </div>
+                <div className="body">{value.postText}</div>
+                <div className="footer">{value.username}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
