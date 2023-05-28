bccac9e182d2c7fd929211e9bd250c618aa1dded
Niya Shiyas <niyashiyas03@gmail.com>
2023-05-28 13:10:30 +0530

    server changed

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,7 +4,11 @@ function Profile() {
   return (
     <div className="ml-5 mr-5 mt-5">
       <h1>Profile</h1>
-      <p>This is the profile page of the user</p>
+      <img
+        className=""
+        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"
+        alt="profile"
+      ></img>
     </div>
   );
 }
